413b2a90427edf1ae97f9bd7fafa970059dc6318
wk <somnuk.wk@gmail.com>
2020-04-24 20:19:44 +0700

    Initial commit

---
+++ src/Generator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NSwag;
+using NSwag.CodeGeneration.CSharp;
+
+namespace Generator {
+    class Program {
+        static async Task Generate(string source, string target, string name, bool repeat = false) {
+            var document = await OpenApiYamlDocument.FromUrlAsync(source);
+
+            var settings = new CSharpClientGeneratorSettings {
+                ClassName = "{controller}Client",
+                ExposeJsonSerializerSettings = true,
+                CSharpGeneratorSettings = {
+                    Namespace = name
+                }
+            };
+
+            var generator = new CSharpClientGenerator(document, settings);
+            var code = generator.GenerateFile();
+            File.WriteAllText(target, code);
+        }
+
+        static async Task Main(string[] args) {
+            await Generate(
+                "https://raw.githubusercontent.com/Alfresco/rest-api-explorer/master/src/main/webapp/definitions/alfresco-core.yaml",
+                "src/AlfrescoClient/AlfrescoCore.cs",
+                "AlfrescoClient.AlfrescoCore");
+
+            await Generate(
+                "https://raw.githubusercontent.com/Alfresco/rest-api-explorer/master/src/main/webapp/definitions/alfresco-auth.yaml",
+                "src/AlfrescoClient/AlfrescoAuth.cs",
+                "AlfrescoClient.AlfrescoAuth",
+                true);
+
+            await Generate(
+                "https://raw.githubusercontent.com/Alfresco/rest-api-explorer/master/src/main/webapp/definitions/alfresco-search.yaml",
+                "src/AlfrescoClient/AlfrescoSearch.cs",
+                "AlfrescoClient.AlfrscoSearch",
+                true);
+        }
+    }
+
+}
